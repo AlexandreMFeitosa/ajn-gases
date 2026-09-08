224b01fb2d31def8f44871024539a184b5c15454
Alexandre Moraes Feitosa <alexandre.feitosa@mt.transportes.gov.br>
2026-09-08 13:24:41 -0300

    Criado um novo componente SOBRE

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
 import Hero from "../../components/sections/Hero";
 import FeaturedProducts from "../../components/sections/FeaturedProducts";
 import Sectors from "../../components/sections/Sectors";
+import About from "../../components/sections/About";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <br/>
+      <hr/>
+      <br/>
       <FeaturedProducts />
+      <br/>
+      <hr/>
+      <br/>
       <Sectors />
+      <br/>
+      <hr/>
+      <br/>
+      <About />
     </div>
   );
 }
